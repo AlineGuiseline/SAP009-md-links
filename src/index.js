03b4cff77ee89382e8b4817893a57107396e7571
Aline Guiseline <alineguiseline@gmail.com>
2023-04-28 01:37:38 -0300

    Criação da função de estatísticas

--- src/index.js
+++ src/index.js
@@ -1,65 +1,231 @@
-/*module.exports = () => {
-  // ...
-};
-*/
-
 const fs = require('fs')
 const chalk = require('chalk');
+const {checkStatusOfLinks, validatedList} = require('./validation.js')
 
-function extractLinks(text) {
+function extractLinks(filePath) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-  const catchRegex = [...text.matchAll(regex)]
-  const result = catchRegex.map(captura => ({[captura[1]]: captura[2]}));
-  return result.length !== 0 ? result : chalk.red('Não há links no arquivo');
-}
 
-function handleError(error) {
-  throw new Error(chalk.red(error.code, 'Ocorreu um erro durante a execução. Favor tentar novamente'));
-}
-
-// Promessa com then/catch
-function showFile(filePath) {
   return fs.promises.readFile(filePath, 'utf-8')
-    .then((text) => extractLinks(text))
-    .catch((error) => handleError(error))
-}
-
-module.exports = {showFile, handleError};
-
-/*
-// Promessa com new Promise()
-function showFile(filePath) {
-  fs.readFile(filePath, 'utf-8', (error, text) => {
-    return new Promise((resolve, reject) => {
-      if (!text) {
-        reject(handleError(error))
-      }
-        resolve(console.log(extractLinks(text)))
+    .then((text) => {
+      const captures = [...text.matchAll(regex)];
+      const results = captures.map((capture) => ({
+        file: filePath,
+        href: capture[2],
+        text: capture[1],
+      }));
+      return results;
     })
-  })
+    .catch((error) => {throw new Error(chalk.red(error.code, 'Não existe arquivo no diretório indicado'))});
 }
 
-showFile('./src/teste.md')
+//Melhor opção MEU DEUS EU NÃO AGUENTO MAIS
+function mdLinks(path, options) {
+  if (fs.lstatSync(path). isDirectory()) {
+    fs.promises.readdir(path)
+    .then(files => {
+      files.forEach((fileName) => {
+        extractLinks(`${path}/${fileName}`)
+        .then(result => {
+          if (options.stats && options.validate) {
+            checkStatusOfLinks(result)
+            .then(({totalLinks, uniqueLinks, brokenLinks}) => {
+              console.log(chalk.yellow(`Estatísticas dos links do arquivo: ${chalk.black.bgGreen(fileName)}\n${chalk.magenta('Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('Unique:')} ${chalk.blue(uniqueLinks)}\n${chalk.blue('Broken:')} ${chalk.blue(brokenLinks)}`))
+            })
+          } else if (options.stats) {
+            checkStatusOfLinks(result)
+              .then(({totalLinks, uniqueLinks}) => {
+                console.log(chalk.yellow(`Estatísticas dos links do arquivo: ${chalk.underline.yellow(fileName)}\n${chalk.magenta('Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('Unique:')} ${chalk.blue(uniqueLinks)}`))
+              })
+          } else if (options.validate) {
+            validatedList(result)
+             .then((category) => {
+              console.log(chalk.yellow('--- Lista de links válidos:'),
+              chalk.yellow(fileName)),
+              category.map(({text, href, file, status}) => console.log(`${chalk.blue(file)} | ${chalk.blue(text)} | ${chalk.blue(href)} | ${status} `))});
+          } else if (result.length === 0) {
+              console.log(chalk.red(`Não há links no arquivo ${chalk.underline.red(fileName)}`))
+          } else {
+              console.log(chalk.black.bgGreen('--- Lista de links:'),
+              chalk.yellow(fileName)),
+              result.map(({text, href, file}) => console.log(`${chalk.green(text)} \n href: ${chalk.blue(href)} \n pasta: ${chalk.magenta(file)}`));
+          }
+        })
+      })
+    })
+  } else {
+    extractLinks(path)
+      .then(links => {
+        if (options.stats && options.validate) {
+          checkStatusOfLinks(links)
+            .then(({ totalLinks, uniqueLinks, brokenLinks }) => {
+              console.log(chalk.yellow(`\n Estatísticas dos links:\n${chalk.magenta('- Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('- Unique:')} ${chalk.blue(uniqueLinks)}\n${chalk.cyan('- Broken:')} ${chalk.cyan(brokenLinks)}`))
+            })
+        } else if (options.stats) {
+          checkStatusOfLinks(links)
+            .then(({ totalLinks, uniqueLinks }) => {
+              console.log(chalk.yellow(`\n Estatísticas dos links: \n${chalk.magenta('- Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('- Unique:')} ${chalk.blue(uniqueLinks)}`))
+            })
+        } else if (options.validate) {
+          validatedList(links)
+            .then((category) => {
+            console.log(chalk.yellow('--- Lista de links válidos:')),
+            category.map(({text, href, file, status}) => console.log(`${chalk.blue(file)} | ${chalk.blue(text)} | ${chalk.blue(href)} | ${status} `))});
+        } else if (links.length === 0) {
+            console.log(chalk.red('\n', 'Não há links no arquivo'))
+        } else {
+          console.log(chalk.black.bgGreen('--- Lista de links:')),
+          links.map(({text, href, file}) => console.log(`${chalk.green(text)} \n href: ${chalk.blue(href)} \n pasta: ${chalk.magenta(file)}`));
+        }
+      })
+  } 
+}
+
+//Outra tentativa
+/*
+function mdLinks(path, options) {
+  if (fs.lstatSync(path). isDirectory()) {
+    fs.promises.readdir(path)
+    .then(files => {
+      files.forEach((fileName) => {
+        extractLinks(`${path}/${fileName}`)
+        .then(result => {
+          if (options.stats && options.validate) {
+            checkStatusOfLinks(result)
+            .then(({totalLinks, uniqueLinks, brokenLinks}) => {
+              console.log(chalk.yellow(`Estatísticas dos links do arquivo: ${chalk.black.bgGreen(fileName)}\n${chalk.magenta('Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('Unique:')} ${chalk.blue(uniqueLinks)}\n${chalk.blue('Broken:')} ${chalk.blue(brokenLinks)}`))
+            })
+            .catch((error) => {
+              console.error(error)
+            });
+          } else if (options.stats) {
+            checkStatusOfLinks(result)
+              .then(({totalLinks, uniqueLinks}) => {
+                console.log(chalk.yellow(`Estatísticas dos links do arquivo: ${chalk.underline.yellow(fileName)}\n${chalk.magenta('Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('Unique:')} ${chalk.blue(uniqueLinks)}`))
+              })
+              .catch((erro) => {
+                console.error(erro);
+              });
+          } else if (options.validate) {
+            //checkStatusOfLinks(result)
+            validatedList(result)
+             .then((category) => {
+              console.log(chalk.yellow('--- Lista de links válidos:'),
+              chalk.yellow(fileName)),
+              category.map(({text, href, file, status}) => console.log(`${chalk.blue(file)} | ${chalk.blue(text)} | ${chalk.blue(href)} | ${status} `))});
+          } else if (result.length === 0) {
+              console.log(chalk.red(`Não há links no arquivo ${chalk.underline.red(fileName)}`))
+          } else {
+              console.log(chalk.black.bgGreen('--- Lista de links:'),
+              chalk.yellow(fileName)),
+              result.map(({text, href, file}) => console.log(`${chalk.green(text)} \n href: ${chalk.blue(href)} \n pasta: ${chalk.magenta(file)}`));
+          }
+        })
+      })
+    })
+  } else {
+    extractLinks(path)
+      .then(links => {
+        if (options.stats && options.validate) {
+          checkStatusOfLinks(links)
+            .then(({ totalLinks, uniqueLinks, brokenLinks }) => {
+              console.log(chalk.yellow(`\n Estatísticas dos links:\n${chalk.magenta('- Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('- Unique:')} ${chalk.blue(uniqueLinks)}\n${chalk.cyan('- Broken:')} ${chalk.cyan(brokenLinks)}`))
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        } else if (options.stats) {
+          checkStatusOfLinks(links)
+            .then(({ totalLinks, uniqueLinks }) => {
+              console.log(chalk.yellow(`\n Estatísticas dos links: \n${chalk.magenta('- Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('- Unique:')} ${chalk.blue(uniqueLinks)}`))
+            })
+            .catch((erro) => {
+              console.error(erro);
+            });
+        } else if (options.validate) {
+          validatedList(links)
+            .then((category) => {
+            console.log(chalk.yellow('--- Lista de links válidos:')),
+            category.map(({text, href, file, status}) => console.log(`${chalk.blue(file)} | ${chalk.blue(text)} | ${chalk.blue(href)} | ${status} `))});
+        } else if (links.length === 0) {
+            console.log(chalk.red('\n', 'Não há links no arquivo'))
+        } else {
+          printList(options.validate, links);
+        }
+      })
+  } 
+} 
 */
+
+
+// Outra tentativa EU JÁ NEM SEI MAIS O QUE EU TÔ FAZENDO
+/*
+function mdLinks(path, options) {
+  if (fs.lstatSync(path). isDirectory()) {
+    fs.promises.readdir(path)
+    .then(files => {
+      files.forEach((fileName) => {
+        extractLinks(`${path}/${fileName}`)
+        .then(result => {
+          if (options.stats && options.validate) {
+            checkStatusOfLinks(result)
+            .then(({totalLinks, uniqueLinks, brokenLinks}) => {
+              console.log(chalk.yellow(`Estatísticas dos links do arquivo: ${chalk.black.bgGreen(fileName)}\n${chalk.magenta('Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('Unique:')} ${chalk.blue(uniqueLinks)}\n${chalk.blue('Broken:')} ${chalk.blue(brokenLinks)}`))
+            })
+          } else if (options.stats) {
+            checkStatusOfLinks(result)
+              .then(({totalLinks, uniqueLinks}) => {
+                console.log(chalk.yellow(`Estatísticas dos links do arquivo: ${chalk.underline.yellow(fileName)}\n${chalk.magenta('Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('Unique:')} ${chalk.blue(uniqueLinks)}`))
+              })
+          } else if (options.validate) {
+            //checkStatusOfLinks(result)
+            validatedList(result)
+             .then((category) => {
+              console.log(chalk.yellow('--- Lista de links válidos:'),
+              chalk.yellow(fileName)),
+              category.map(({text, href, file, status}) => console.log(`${chalk.blue(file)} | ${chalk.blue(text)} | ${chalk.blue(href)} | ${status} `))});
+          } else if (result.length === 0) {
+              console.log(chalk.red(`Não há links no arquivo ${chalk.underline.red(fileName)}`))
+          } else {
+              console.log(chalk.black.bgGreen('--- Lista de links:'),
+              chalk.yellow(fileName)),
+              result.map(({text, href, file}) => console.log(`${chalk.green(text)} \n href: ${chalk.blue(href)} \n pasta: ${chalk.magenta(file)}`));
+          }
+        })
+      })
+    })
+  } else {
+    extractLinks(path)
+      .then(links => {
+        if (options.stats && options.validate) {
+          checkStatusOfLinks(links)
+            .then(({ totalLinks, uniqueLinks, brokenLinks }) => {
+              console.log(chalk.yellow(`\n Estatísticas dos links:\n${chalk.magenta('- Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('- Unique:')} ${chalk.blue(uniqueLinks)}\n${chalk.cyan('- Broken:')} ${chalk.cyan(brokenLinks)}`))
+            })
+        } else if (options.stats) {
+          checkStatusOfLinks(links)
+            .then(({ totalLinks, uniqueLinks }) => {
+              console.log(chalk.yellow(`\n Estatísticas dos links: \n${chalk.magenta('- Total:')} ${chalk.magenta(totalLinks)}\n${chalk.blue('- Unique:')} ${chalk.blue(uniqueLinks)}`))
+            })
+        } else if (options.validate) {
+          validatedList(links)
+            .then((category) => {
+            console.log(chalk.yellow('--- Lista de links válidos:')),
+            category.map(({text, href, file, status}) => console.log(`${chalk.blue(file)} | ${chalk.blue(text)} | ${chalk.blue(href)} | ${status} `))});
+        } else if (links.length === 0) {
+            console.log(chalk.red('\n', 'Não há links no arquivo'))
+        } else {
+          printList(options.validate, links);
+        }
+      })
+  } 
+} */
+
+module.exports = {mdLinks};
+
+
+
 
 // lib fs
 //1º parâmetro: caminho do arquivo que ele vai ler
 //2º parâmetro: qual é o encoding de caracteres que ele vai encontrar (ex.: UTF-8)
 //3º parâmetro: função callback (erro e retorno)
-
-/*
-//exibir o arquivo no console
-const file = 'src/teste.md'
-fsp.readFile(file, 'utf-8').then(console.log).catch(console.error)
-
-//mostrar a extensão do arquivo
-const test = path.extname('thumb.png')
-console.log(test)
-
-//mostrar os arquivos dentro de uma pasta
-fsp.readdir(path.join(__dirname)).then(files => {
-  const mdFiles = files.filter(file => file.endsWith('.md'))  
-
-  console.log(mdFiles)
-}).catch(console.error)
-*/
--- src/validation.js
+++ src/validation.js
@@ -1,5 +1,129 @@
-function listaValidada(listaDeLinks) {
-    return 'entrou na função';
+const chalk = require("chalk");
+
+function checkStatus(listOfURLs) {
+  return Promise.all(
+    listOfURLs.map((url) => {
+      return fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return `${chalk.green('OK')} | ${chalk.green(response.status)}`
+        } else {
+          return `${chalk.red('FAIL')} |  ${chalk.red(response.status)}`
+        }
+      })
+      .catch(error => {
+        if (error.cause.code === 'ENOTFOUND') {
+          return chalk.red('O link não foi encontrado');
+        } else {
+          return chalk.red('Ops... Ocorreu algum erro');
+        }
+      })
+    })
+  )
 }
 
-module.exports = listaValidada
+function validatedList (listOfLinks) {
+  return checkStatus(listOfLinks.map((targetLink) => targetLink.href))
+  .then((status) => {
+    return listOfLinks.map((objeto, indice) => ({
+      ...objeto,
+      status: status[indice]
+    }));
+  });
+}
+
+function checkStatusOfLinks (listOfLinks) {
+  const totalLinks = listOfLinks.length;
+  const uniqueLinks = new Set(listOfLinks.map((targetLink) => targetLink.href)).size;
+
+  return checkStatus(listOfLinks.map((targetLink) => targetLink.href))
+  .then((linkStatus) => {
+    const brokenLinks = linkStatus.filter(status => status.startsWith(chalk.red('FAIL'))).length;
+    return {totalLinks, uniqueLinks, brokenLinks};
+  })
+  .catch((error) => {
+    console.log(error)
+  });
+}
+
+module.exports = {checkStatus, validatedList, checkStatusOfLinks}
+
+/*
+function extraiLinks(arrLinks) {
+  return arrLinks.map((objetoLink) => Object.values(objetoLink).join());
+}
+*/
+// Object.values serve para que consigamos extrair apenas valores para dentro de um array.
+/*
+function handleError(error) {
+    if (error.cause.code === 'ENOTFOUND') {
+        return 'link não encontrado';
+    } else {
+        return 'ocorreu um erro'
+    }
+}
+
+function checaStatus(listaURLs) {
+  return Promise.all(
+    listaURLs.map((url) => {
+      return fetch(url)
+        .then((response) => response.status)
+        .catch((error) => {
+          return handleError(error)
+            //console.error(chalk.red(`Erro ao buscar a URL ${url}: ${error}`));
+          //throw error;
+        });
+    })
+  );
+}
+function listaValidada(listaDeLinks) {
+    const links = extraiLinks(listaDeLinks);
+    
+    return checaStatus(links)
+      .then((status) => {
+        return listaDeLinks.map((objeto, indice) => ({
+          ...objeto,
+          status: status[indice]
+        }));
+      })
+      /*.catch((error) => {
+        console.error(`Erro ao validar lista de links: ${error}`);
+        throw error;
+      });
+  }
+  */
+
+  /*
+  function checaStatus (listaURLs) {
+    return Promise.all(
+        listaURLs.map((url) => {
+        return fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return `OK | ${response.status}`
+            } else {
+                return `FAIL | ${response.status}`
+            }
+        })
+        .catch(error => {
+            if (error.cause.code === 'ENOTFOUND') {
+                return 'Link não encontrado';
+            } else { 
+                return 'Ocorreu algum erro';
+            }
+        })
+    })) 
+}
+
+function listaValidada(arrLinks) {
+    return checaStatus(arrLinks.map((objetoLink) => objetoLink.href))
+    .then((status) => {
+        return arrLinks.map((objeto, indice) => ({
+            ...objeto,
+            status: status[indice]
+        }));
+    });
+}
+
+module.exports = listaValidada;
+*/
