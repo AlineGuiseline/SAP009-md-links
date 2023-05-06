761d4393448f5505e34d641b1fe23db151c6be17
Aline Guiseline <alineguiseline@gmail.com>
2023-05-05 21:24:05 -0300

    Início da refatoração - index.js

--- src/index.js
+++ src/index.js
@@ -28,38 +28,15 @@ function mdLinks(path, options) {
     return;
   }
    if (isDirectory(path)) {
-    fs.promises.readdir(path)
+    return fs.promises.readdir(path)
       .then(files => {
-        files.forEach((fileName) => {
+        return Promise.all(files.map((fileName) => {
           const filePath = `${path}/${fileName}`;
-          extractLinks(filePath)
+          return extractLinks(filePath)
             .then(result => {
-              if (options.stats && options.validate) {
-                checkStatusOfLinks(result)
-                .then(({totalLinks, uniqueLinks, brokenLinks}) => {
-                  console.log(chalk.yellow(`--- Estatísticas totais dos links do arquivo: ${chalk.black.bgGreen(fileName)}\n${chalk.magenta('Total:')} ${chalk.magenta(totalLinks)}\n${chalk.cyan('Unique:')} ${chalk.cyan(uniqueLinks)}\n${chalk.red('Broken:')} ${chalk.red(brokenLinks)}`))
-                })
-              } else if (options.stats) {
-                checkStatusOfLinks(result)
-                  .then(({totalLinks, uniqueLinks}) => {
-                    console.log(chalk.yellow(`--- Estatísticas dos links do arquivo: ${chalk.black.bgGreen(fileName)}\n${chalk.magenta('Total:')} ${chalk.magenta(totalLinks)}\n${chalk.cyan('Unique:')} ${chalk.cyan(uniqueLinks)}`))
-                  })
-              } else if (options.validate) {
-                validatedList(result)
-                  .then((category) => {
-                    console.log(chalk.yellow('--- Lista de links válidos:'),
-                    chalk.black.bgGreen(fileName)),
-                    category.map(({text, href, file, status}) => console.log(`${chalk.blue(file)} | ${chalk.magenta(text)} | ${chalk.cyan(href)} | ${status} `))
-                  });
-              } else if (result.length === 0) {
-                  console.log(chalk.red(`Não há links no arquivo ${chalk.underline.red(fileName)}`))
-              } else {
-                  console.log(chalk.black.bgGreen('--- Lista de links:'),
-                  chalk.yellow(fileName)),
-                  result.map(({text, href, file}) => console.log(`${chalk.green(text)} \n href: ${chalk.blue(href)} \n pasta: ${chalk.magenta(file)}`));
-              }
+              return validateOptions (options, result)
             })
-        });
+        }));
       });
   }
   if (isFile(path)) {
@@ -67,33 +44,24 @@ function mdLinks(path, options) {
       extractLinks(path);
       console.log(chalk.red('Extensão inválida'));
     } else {
-      extractLinks(path)
+      return extractLinks(path)
         .then(links => {
-          if (options.stats && options.validate) {
-            checkStatusOfLinks(links)
-              .then(({ totalLinks, uniqueLinks, brokenLinks }) => {
-                console.log(chalk.yellow(`--- Estatísticas totais dos links:\n${chalk.magenta('- Total:')} ${chalk.magenta(totalLinks)}\n${chalk.cyan('- Unique:')} ${chalk.cyan(uniqueLinks)}\n${chalk.red('- Broken:')} ${chalk.red(brokenLinks)}`))
-              })
-          } else if (options.stats) {
-            checkStatusOfLinks(links)
-              .then(({ totalLinks, uniqueLinks }) => {
-                console.log(chalk.yellow(`--- Estatísticas dos links: \n${chalk.magenta('- Total:')} ${chalk.magenta(totalLinks)}\n${chalk.cyan('- Unique:')} ${chalk.cyan(uniqueLinks)}`))
-              })
-          } else if (options.validate) {
-            validatedList(links)
-              .then((category) => {
-              console.log(chalk.yellow('--- Lista de links válidos:')),
-              category.map(({text, href, file, status}) => console.log(`${chalk.blue(file)} | ${chalk.magenta(text)} | ${chalk.cyan(href)} | ${status} `))
-              });
-          } else if (links.length === 0) {
-            console.log(chalk.red('\n', 'Não há links no arquivo indicado'))
-          } else {
-            console.log(chalk.black.bgGreen('--- Lista de links:')),
-            links.map(({text, href, file}) => console.log(`${chalk.green(text)} \n href: ${chalk.blue(href)} \n pasta: ${chalk.magenta(file)}`));
-        }
+          return validateOptions (options, links)
         });
      }
   }
+}
+
+function validateOptions (options, links) {
+  if (options.stats && options.validate) {
+    return checkStatusOfLinks(links)
+   } else if (options.stats) {
+    return checkStatusOfLinks(links)
+   } else if (options.validate) {
+     return validatedList(links)
+   } else {
+     return links
+   }
 }
 
 module.exports = {isFile, isDirectory, mdLinks};
